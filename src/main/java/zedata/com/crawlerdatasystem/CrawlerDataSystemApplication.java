5f63565dfd272b38221097ef0cc5e0dda3ed10e3
Mr-Glacier <K15100415112@163.com>
2024-05-30 14:43:37 +0800

    Initial commit

---
+++ src/main/java/zedata/com/crawlerdatasystem/CrawlerDataSystemApplication.java
@@ -0,0 +1,13 @@
+package zedata.com.crawlerdatasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerDataSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerDataSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zedata/com/crawlerdatasystem/CrawlerDataSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package zedata.com.crawlerdatasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerDataSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
